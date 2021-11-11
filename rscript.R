c769ff046845fe362c47c35b848723c849aaa8e8
yangyong ye <mouse107.5@gmail.com>
2021-11-11 20:46:05 +0800

    update file structure

---
+++ rscript.R
@@ -0,0 +1,6 @@
+devtools::install_github("datalorax/slidex")
+library(slidex)
+library(slidex)
+pptx <- system.file("examples", "slidedemo.pptx", package = "slidex")
+
+convert_pptx(path ="~/R/pptgit/paper_guide/8其它.pptx", author = "叶阳永",out_dir = "paper/")
